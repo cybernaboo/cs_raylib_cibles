ee46eea0b2b8f502a9f6f6f60578de788618c424
nabil aici <cybernaboo@yahoo.fr>
2023-07-27 19:38:57 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Numerics;
+using Raylib_CsLo;
+using static Raylib_CsLo.Raylib;
+public class Program
+{
+    public const int screenWidth = 600;
+    public const int screenHeight = 600;
+    static void Main()
+    {
+        Font font = GetFontDefault();       // Get default system font
+        Game game = new Game();
+        game.Print();
+
+        InitWindow(screenWidth, screenHeight, "Cowboy Game (Cybernaboo)");
+        SetTargetFPS(60);
+        // Mie en pause du programme en attendant qu'une touche clavier soit appuyée
+        // Console.WriteLine("une touche pour continuer");
+        // Console.ReadKey(true);
+
+        while (!WindowShouldClose())
+        {
+            // if (IsMouseButtonPressed(MouseButton.MOUSE_BUTTON_LEFT))
+            // {
+            //     int mouseX = GetMouseX();
+            //     int mouseY = GetMouseY();
+            //     System.Console.WriteLine($"Clic souris aux coordonnées : ({mouseX}, {mouseY})");
+            // }
+            BeginDrawing();
+            ClearBackground(WHITE);
+            DrawTextEx(font, "Score", new Vector2(5.0f, 5.0f), 25, 2, GREEN);
+            DrawRectangle(0, 30, screenWidth, 2, BLACK);
+            game.Draw();
+            EndDrawing();
+        }
+        UnloadFont(font);    // TTF Font unloading
+        CloseWindow();
+    }
+}
---
+++ components/game.cs
@@ -0,0 +1,57 @@
+using System.Numerics;
+using Raylib_CsLo;
+using static Raylib_CsLo.Raylib;
+
+
+public class Game
+{
+    List<Vector2> targetList = new List<Vector2>();
+
+    static int gameWidth = 500;
+    static int gameHeight = 620;
+    static int targetWidth = 50;
+    static int targetHeight = 50;
+    private Texture texture;
+    private Image image;
+
+    public Game()
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        Console.WriteLine("Répertoire en cours : " + currentDirectory);
+        string filePath = "./images/food.png"; // Remplacez ceci par le chemin d'accès complet de votre fichier
+        if (File.Exists(filePath))
+        {
+            Console.WriteLine("fichier trouvé");
+        }
+        image = LoadImage("./images/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        CreateRandomTarget();
+    }
+    ~Game()
+    {
+        UnloadTexture(texture);
+    }
+    void CreateRandomTarget()
+    {
+        Vector2 newTarget = new Vector2(GetRandomValue(0, gameWidth - targetWidth), GetRandomValue(0, gameHeight - targetHeight));
+        targetList.Add(newTarget);
+    }
+
+    public void Print()
+    {
+        foreach (Vector2 target in targetList)
+        {
+            Console.WriteLine($"Target : {target}");
+        }
+    }
+    public void Draw()
+    {
+        foreach (Vector2 target in targetList)
+        {
+            Color darkGreen = new Color(43, 51, 24, 255);
+            // DrawTexture(texture, (int)target.X, (int)target.Y, darkGreen);
+            DrawTexture(texture, 650, 650, darkGreen);
+        }
+    }
+}
